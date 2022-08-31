6526d053353930f3b6548e96e0002db579cc7b99
Code 41 <washingtonbenton@gmail.com>
2022-08-31 10:20:01 -0400

    updated server side w utils

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,41 +1,51 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Category {
+    type User {
         _id: ID
-        categoryName: String
-    }
-    
+        username: String
+        email: String
+        password: String
+        requests: [Request]
+}
+
     type Request {
         _id: ID
         category: Category
         title: String
         description: String
         details: String
+        requestAuthor: String
         createdAt: String
+}
+
+    type Category {
+        _id: ID
+        categoryName: String
     }
 
-    type User {
-        _id: ID
-        username: String
-        email: String
-        password: String
+    type Auth {
+        token: ID!
+        user: User
     }
+
 
     type Query {
-        requests: [Request]
+        me: User
+        user(username: String!): User
         categories: [Category]
+        requests: [Request]
         catrequests(category: ID!): [Request]
-        user(userId: ID!): User
     }
 
     type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(email: String!, username: String!, password: String!): Auth
+        deleteUser(userId: ID!): User
         addRequest(title: String!, description: String!, details: String!, category: ID!): Request
         updateRequest(description: String! details: String!): Request
         deleteRequest(requestId: ID!): Request
-        addUser(email: String!, username: String!, password: String!): User
-        updateUser(email: String!): User
-        deleteUser(userId: ID!): User
+
     }
 `;
 
