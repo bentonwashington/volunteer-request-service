bffe08e31f4a8f37b0170b698d5d5a9245ec0806
jhenry <107387628+j-henryrowland@users.noreply.github.com>
2022-08-31 13:38:06 -0400

    feed push

--- client/src/components/Feed/feed.jsx
+++ client/src/components/Feed/feed.jsx
@@ -1,3 +1,25 @@
 import React from 'react'
-import '../../App.css'
+import '../../App/css'
+;
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function WithHeaderStyledExample() {
+  return (
+    <Card>
+      <Card.Header as="h5">Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default WithHeaderStyledExample;
+
 
