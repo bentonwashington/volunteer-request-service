5521c001f966dd61624b23c2c8a399b378f6ef35
Peru PC <paulocondori1@gmail.com>
2022-08-31 22:53:51 -0400

    first draft

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../App.css'
 import '../Login/login.css'
 
+import {LOGIN_USER} from '../../utils/mutations'
+import {useMutation } from '@apollo/client'
+
+
+
 const Login = () => {
+
+  const [formData, setFormData] = useState({email: '', password: ''})
+const handleSubmit = (event) => {
+  event.preventDefault()
+  console.log(formData)
+}
+
         return (
           <div className='container'> 
-    <form id='shapeUp'>
+
+    <form onSubmit={handleSubmit} id='shapeUp'>
           <h1 id='Welcome'> Welcome Hero </h1>
           <h1> Let's Login </h1>
 
            <div class="mb-3">
            <label for="exampleInputEmail1" class="form-label">Email address</label>
-           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+           <input type="email" value={formData.email} onChange={(event) => setFormData({email: event.target.email, ...formData})} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1"/>
+        <input type="password" value={formData.password} onChange={(event) => setFormData({password : event.target.password})} class="form-control" id="exampleInputPassword1"/>
       </div>
              <div class="mb-3 form-check">
              {/* <input type="checkbox" class="form-check-input" id="exampleCheck1"/> */}
